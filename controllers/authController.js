83ea739aa48c7e25fc622330fab0b56b3f1b825d
Roosevelt <LarkinsRo@gmail.com>
2021-06-01 17:24:55 -0400

    work

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,25 @@
 const User = require('../models/Users');
 const jwt = require('jsonwebtoken');
 
+// handle errors
 
 const handleErrors = (err) => {
     console.log(err.message, err.code);
     let errors = {email: '', password:''};
+
+    // incorrect email
+    if (err.message === 'incorrect email') {
+        errors.email = 'that email is not registered';
+    }
+
+    // incorrect email
+    if (err.message === 'incorrect password') {
+        errors.password = 'that password is incorrect';
+    }
+
+
+
+  // duplicate error code
 
   if (err.code ===11000){
       errors.email = 'that email is already in use!';
@@ -12,6 +27,7 @@ const handleErrors = (err) => {
   }
 
 
+  // validation errors
 
     if(err.message.includes('user validation failed')){
         Object.values(err.errors).forEach(({properties}) => {
@@ -62,11 +78,23 @@ module.exports.signup_post = async (req, res) => {
 
 module.exports.login_post = async (req, res) => {
     const { email, password } = req.body;
-    
+  
 
-    console.log(email, password);
+    try {
+        const user = await User.login(email, password);
+        const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+        res.status(201).json({ user: user._id });
 
-    res.send('user login');
+    }
+    catch (err) {
+        const errors = handleErrors(err);
+     res.status(400).json({errors});
+    }
     
+}
+module.exports.logout_get = (req, res) => {
+    res.cookie('jwt', '', { maxAge: 1 });
+    res.redirect('/');
 }
 
